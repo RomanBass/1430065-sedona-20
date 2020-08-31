d411b757210e537cd1ea14966c16ad6beacbab18
RomanBass <49683918+RomanBass@users.noreply.github.com>
2020-08-31 14:23:55 +0300

    2020-0831-14-23

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const del = require("del");
 
 // Styles
 
@@ -16,8 +19,10 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
@@ -49,3 +54,37 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+// Copy Files
+
+const copy = () => {
+  return gulp.src([
+      "source/fonts/**/*.{woff, woff2}",
+      "source/img/**",
+      "source/js/**",
+      "source/*.ico"
+  ], {
+      base: "source"
+  })
+  .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
+
+//Clear Folder
+
+const clean = () => {
+    return del("build");
+};
+
+exports.clean = clean;
+
+//New Run Build
+
+const build = gulp.series(
+    "clean",
+    "copy"
+);
+
+exports.build = build;
+
